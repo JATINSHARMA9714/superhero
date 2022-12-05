6212f5fa1e64cf1d93d72172823deb4eb802aa5e
unknown <jatinsharma6204063696@gmail.com>
2022-12-05 15:29:18 +0530

    javascript done

--- hero.js
+++ hero.js
@@ -2,23 +2,34 @@ const baseurl='https://www.superheroapi.com/api.php/2918041138490868'
 const images=document.getElementById('image')
 const input=document.getElementById('typedtext')
 const search=document.getElementById('searchsuperhero')
+const getherostats=(character)=>{
+    const statsinfo=Object.keys(character.powerstats).map(status=>{
+        return `<p>${status} : ${character.powerstats[status]}</p>`
+    })
+    // console.log(statsinfo.join(''))
+    return statsinfo.join('')
+}
 const getsearchedhero=(named)=>{
     // console.log('hello')
 fetch(`${baseurl}/search/${named}`)
 .then(response=>response.json())
 .then(json=>{
-    console.log(json.results[0])
-    images.innerHTML=`<img src='${json.results[0].image.url}' height=200 width=200/>`
-    images.innerHTML+=`${json.results[0].name}`
+    // console.log(json.results[0].powerstats)
+    let stats=getherostats(json.results[0])
+    //     const name=`<h2>${json.name}</h2>`
+    //    images.innerHTML=`${name}<img src='${json.image.url}' height=200 width=200/>${stats}`
+   let name=`<h2>${json.results[0].name}</h2>`
+    images.innerHTML=`${name}<img src='${json.results[0].image.url}' height=200 width=200/>${stats}`
 })
 }
 const getrandomhero=(id)=>{
     fetch(`${baseurl}/${id}`)
     .then(response=>response.json())
     .then(json=>{
-        console.log(json)
-       images.innerHTML=`<img src='${json.image.url}' height=200 width=200/>`
-       images.innerHTML+=`${json.name}`
+        // console.log(json)
+        const stats=getherostats(json)
+        const name=`<h2>${json.name}</h2>`
+       images.innerHTML=`${name}<img src='${json.image.url}' height=200 width=200/>${stats}`
     })
 }
 
@@ -29,7 +40,7 @@ randombutton.addEventListener("click",function(){
 })
 search.addEventListener("click",function(){
     const data=input.value
-    console.log(data)
+    // console.log(data)
     // console.log('hello')
     getsearchedhero(data)
 })
